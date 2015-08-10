d5604cce8d5de7464f13ea4a52792a9d8978a102
ajayj <ajay.jadhav@talentica.com>
2015-08-10 20:43:21 +0530

    Copy pasted angularjs rest demo project

---
+++ angularjs_demo_rest-master/src/main/java/com/angularjs/demo/Application.java
@@ -0,0 +1,22 @@
+package com.angularjs.demo;
+
+/**
+ * 
+ * @author SwapnilB
+ * 
+ */
+public class Application
+{
+	private Person person;
+
+	public Person getPerson()
+	{
+		return person;
+	}
+
+	public void setPerson(Person person)
+	{
+		this.person = person;
+	}
+
+}
